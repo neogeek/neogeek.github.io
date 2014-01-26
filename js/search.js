970bc5aa58442eca5e42e89677a26a0a2a12dd57
Neo Geek <neogeek0101@gmail.com>
2014-01-26 13:13:31 -0500

    Added basic search to articles index.

---
+++ js/search.js
@@ -0,0 +1,50 @@
+
+(function () {
+
+    'use strict';
+
+    // Add search filtering to article listing.
+
+    var allArticles = document.querySelectorAll('.article');
+
+    document.querySelector('.search').addEventListener('keyup', function () {
+
+        var searchQuery = [];
+
+        Array.prototype.slice.call(this.value.split(/\s+/)).forEach(function (value) {
+
+            if (value) {
+
+                searchQuery.push('.article a[data-title*="' + value.toLowerCase() + '"]');
+
+            }
+
+        });
+
+        if (searchQuery.length) {
+
+            Array.prototype.slice.call(allArticles).forEach(function (obj) {
+
+                obj.classList.add('hidden');
+
+            });
+
+            Array.prototype.slice.call(document.querySelectorAll(searchQuery.join(', '))).forEach(function (obj) {
+
+                obj.parentNode.classList.remove('hidden');
+
+            });
+
+        } else {
+
+            Array.prototype.slice.call(allArticles).forEach(function (obj) {
+
+                obj.classList.remove('hidden');
+
+            });
+
+        }
+
+    });
+
+}());
