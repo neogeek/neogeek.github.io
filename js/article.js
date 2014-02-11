138555f94547857b9d769ec92d54c2dced777854
Neo Geek <neogeek0101@gmail.com>
2014-02-11 00:13:46 -0500

    Tweaked the 'time to read' math.

--- js/article.js
+++ js/article.js
@@ -7,24 +7,24 @@
 
     var toread = document.querySelector('.toread'),
         minutes = document.querySelector('article').innerText.match(/\s+/g).length / 180,
-        minutes_ceil = Math.ceil(minutes),
+        minutes_floor = Math.floor(minutes),
         minutes_round = Math.round(minutes),
         links = document.querySelectorAll('a[href^="mailto:"]'),
         email_address = window.atob('aGVsbG9Ac2NvdHRkb3hleS5jb20=');
 
     // Display duration to read per article (based on 180 words a minute).
 
-    if (minutes_ceil === minutes_round) {
+    if (minutes_floor === minutes_round) {
 
-        toread.innerHTML = 'about ' + minutes_ceil + ' minutes to read';
+        toread.innerHTML = 'about ' + minutes_round + ' minutes to read';
 
     } else {
 
-        toread.innerHTML = 'less than ' + minutes_ceil + ' minutes to read';
+        toread.innerHTML = 'less than ' + minutes_round + ' minutes to read';
 
     }
 
-    if (minutes_ceil === 1) {
+    if (minutes_round === 1) {
 
         toread.innerHTML = toread.innerHTML.replace('minutes', 'minute');
 
