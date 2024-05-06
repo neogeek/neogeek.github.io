a32c804422f817cea67ad301be046c9ee9fca80c
Scott Doxey <neogeek0101@gmail.com>
2024-05-06 01:06:58 -0400

    Dynamically add posts to main page.

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-## Articles
-
-- [The Simple Game That Took Me 6 Years to Release](the-simple-game-that-took-me-6-years-to-release) - <time>April 29th, 2024</time>
-- [Scott Makes a Game Engine](scott-makes-a-game-engine) - <time>October 27th, 2023</time>
-- [Creating Custom Packages for Unity 2018.3](creating-custom-packages-for-unity-2018.3) - <time>February 1st, 2019</time>
-- [Building an AR App Without Code](/building-an-ar-app-without-code) - <time>December 30th, 2018</time>
-
 ## Current Projects
 
 - [Flip Jacks](https://flipjacksgame.com) - 🃏 A physics based poker game in AR
--- index.mjs
+++ index.mjs
@@ -15,29 +15,31 @@ import config from './_data/config.json' assert { type: 'json' };
 import posts from './_data/posts.json' assert { type: 'json' };
 
 await Promise.all(
-  posts.map(async post => {
-    await writeFileAndMakeDir(
-      dirname(post.path),
-      'build/',
-      html`<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            ${head({
-              title: post.title,
-              subtitle: post.subtitle
-            })}
-          </head>
-          <body>
-            <header>
-              <a href="/">← Back</a>
-            </header>
-            <main class="post">
-              ${renderMarkdown(await readFile(post.path, 'utf8'))}
-            </main>
-          </body>
-        </html>`
-    );
-  })
+  posts
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .map(async post => {
+      await writeFileAndMakeDir(
+        dirname(post.path),
+        'build/',
+        html`<!DOCTYPE html>
+          <html lang="en">
+            <head>
+              ${head({
+                title: post.title,
+                subtitle: post.subtitle
+              })}
+            </head>
+            <body>
+              <header>
+                <a href="/">← Back</a>
+              </header>
+              <main class="post">
+                ${renderMarkdown(await readFile(post.path, 'utf8'))}
+              </main>
+            </body>
+          </html>`
+      );
+    })
 );
 
 await writeFile('build/feed.xml', renderRss(config, posts));
@@ -49,6 +51,30 @@ export default html`<!DOCTYPE html>
     </head>
     <body>
       ${header}
-      <main>${renderMarkdown(await readFile('./README.md', 'utf8'))}</main>
+      <main>
+        <h2>Articles</h2>
+
+        <ul>
+          ${posts
+            .sort(
+              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+            )
+            .map(post => {
+              return html`<li>
+                <a href="${post.path.replace('/README.md', '')}"
+                  >${post.title}</a
+                >
+                -
+                ${new Date(post.date).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+              </li>`;
+            })}
+        </ul>
+
+        ${renderMarkdown(await readFile('./README.md', 'utf8'))}
+      </main>
     </body>
   </html>`;
