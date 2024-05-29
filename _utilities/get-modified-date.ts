33b6dff6c3b69b71a9c83a974d46ef916a96a696
Scott Doxey <neogeek0101@gmail.com>
2024-05-29 16:25:21 -0400

    Updated now page to use last modified from git.

---
+++ _utilities/get-modified-date.ts
@@ -0,0 +1,14 @@
+import util from 'node:util';
+import child_process from 'node:child_process';
+
+const exec = util.promisify(child_process.exec);
+
+const getModifiedDate = async (path: string) => {
+  const { stdout: lastModified } = await exec(
+    `git log -1 --pretty="format:%ci" ${path}`
+  );
+
+  return new Date(lastModified);
+};
+
+export default getModifiedDate;
--- now.ts
+++ now.ts
@@ -1,13 +1,15 @@
-import { readFile, stat } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises';
 
 import { html } from 'onlybuild';
 
 import { Marked } from 'marked';
 
+import getModifiedDate from './_utilities/get-modified-date.js';
+
 import head from './_includes/head.js';
 import footer from './_includes/footer.js';
 
-const { mtime } = await stat('now.md');
+const lastModifiedDate = await getModifiedDate('now.md');
 
 export default html`<!DOCTYPE html>
   <html lang="en">
@@ -29,8 +31,8 @@ export default html`<!DOCTYPE html>
         <p>
           <i
             >Last Updated:
-            <time datetime="${mtime.toString()}"
-              >${mtime.toLocaleDateString('en-us', {
+            <time datetime="${lastModifiedDate.toString()}"
+              >${lastModifiedDate.toLocaleDateString('en-us', {
                 year: 'numeric',
                 month: 'long',
                 day: 'numeric'
