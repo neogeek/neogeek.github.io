7a596e88c91c6716b1c855aff95215f26dd57c0c
Scott Doxey <neogeek0101@gmail.com>
2024-05-30 13:51:08 -0400

    Rebuilt GitHubRepo web component.

--- js/web-components/CopyToClipboard.js
+++ js/web-components/CopyToClipboard.js
@@ -198,11 +198,7 @@
     clipboardButton = () => {
       const button = document.createElement('button');
 
-      const span = document.createElement('span');
-
-      span.appendChild(document.createTextNode('Copied!'));
-
-      button.appendChild(span);
+      button.appendChild(this.createElementWithTextNode('span', 'Copied!'));
 
       button.appendChild(this.clipboardIcon());
 
@@ -219,12 +215,12 @@
     clipboardIcon = () => {
       const xmlNamespace = 'http://www.w3.org/2000/svg';
 
-      const svgClipboard = document.createElementNS(xmlNamespace, 'svg');
+      const svg = document.createElementNS(xmlNamespace, 'svg');
 
-      svgClipboard.setAttribute('height', '1.5em');
-      svgClipboard.setAttribute('viewBox', '0 0 384 512');
+      svg.setAttribute('height', '1.5em');
+      svg.setAttribute('viewBox', '0 0 384 512');
 
-      svgClipboard.appendChild(
+      svg.appendChild(
         document.createComment(
           'Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.'
         )
@@ -237,7 +233,7 @@
         'M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z'
       );
 
-      svgClipboard.appendChild(svgClipboardPath);
+      svg.appendChild(svgClipboardPath);
 
       const svgCheckMarkPath = document.createElementNS(xmlNamespace, 'path');
 
@@ -247,10 +243,27 @@
       svgCheckMarkPath.setAttribute('stroke-linecap', 'round');
       svgCheckMarkPath.setAttribute('stroke-linejoin', 'round');
 
-      svgClipboard.appendChild(svgCheckMarkPath);
+      svg.appendChild(svgCheckMarkPath);
 
-      return svgClipboard;
+      return svg;
     };
+
+    /**
+     * Create an HTMLElement with text.
+     *
+     * @param {string} tagName
+     * @param {string | null} content
+     * @returns {HTMLElement}
+     */
+    createElementWithTextNode(tagName, content = null) {
+      const element = document.createElement(tagName);
+
+      if (content) {
+        element.appendChild(document.createTextNode(content));
+      }
+
+      return element;
+    }
   }
 
   customElements.define('copy-to-clipboard', CopyToClipboard);
--- js/web-components/GitHubRepo.js
+++ js/web-components/GitHubRepo.js
@@ -47,7 +47,7 @@
     h1 {
       font-size: 2em;
       font-weight: 400;
-      text-wrap: wrap;
+      white-space: wrap;
       overflow-wrap: break-word;
     }
 
@@ -88,15 +88,15 @@
     }
   `;
 
-  const createSVGClipboard = () => {
+  const gitHubIcon = () => {
     const xmlNS = 'http://www.w3.org/2000/svg';
 
-    const svgClipboard = document.createElementNS(xmlNS, 'svg');
+    const svg = document.createElementNS(xmlNS, 'svg');
 
-    svgClipboard.setAttribute('height', '1.25em');
-    svgClipboard.setAttribute('viewBox', '0 0 496 512');
+    svg.setAttribute('height', '1.25em');
+    svg.setAttribute('viewBox', '0 0 496 512');
 
-    svgClipboard.appendChild(
+    svg.appendChild(
       document.createComment(
         '!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.'
       )
@@ -109,14 +109,16 @@
       'M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z'
     );
 
-    svgClipboard.appendChild(svgClipboardPath);
+    svg.appendChild(svgClipboardPath);
 
-    return svgClipboard;
+    return svg;
   };
 
   class GitHubRepo extends HTMLElement {
     constructor() {
       super();
+
+      this.attachShadow({ mode: 'open' });
 
       this.owner = this.getAttribute('owner');
       this.name = this.getAttribute('name');
@@ -129,64 +131,74 @@
 
       this.iconUrl =
         this.getAttribute('icon-url') || `https://github.com/${this.owner}.png`;
+
+      this.shadowRoot?.appendChild(this.stylesheet());
+      this.shadowRoot?.appendChild(this.repoAnchor());
     }
-    connectedCallback() {
-      this.attachShadow({ mode: 'open' });
 
-      this.createElementWithTextNode('style', styles);
+    /**
+     * Returns a style tag for use within the web component shadow DOM.
+     *
+     * @returns {HTMLStyleElement}
+     */
+    stylesheet() {
+      const style = document.createElement('style');
 
-      const anchor = this.createElementWithTextNode('a');
+      style.appendChild(document.createTextNode(styles));
 
-      anchor?.setAttribute('href', this.url);
+      return style;
+    }
 
-      const header = this.createElementWithTextNode('header', null, anchor);
+    repoAnchor() {
+      const anchor = document.createElement('a');
 
-      const name = this.createElementWithTextNode(
-        'h1',
-        `${this.owner}/`,
-        header
-      );
+      anchor.setAttribute('href', this.url);
 
-      this.createElementWithTextNode('b', this.name, name);
+      const header = document.createElement('header');
+
+      const name = this.createElementWithTextNode('h1', `${this.owner}/`);
+
+      name.appendChild(this.createElementWithTextNode('b', this.name));
+
+      header.appendChild(name);
 
       if (this.description) {
-        this.createElementWithTextNode('p', this.description, header);
+        header.appendChild(
+          this.createElementWithTextNode('p', this.description)
+        );
       }
 
-      const icon = this.createElementWithTextNode('img', null, anchor);
+      anchor.appendChild(header);
 
-      icon?.setAttribute('src', this.iconUrl);
+      const icon = document.createElement('img');
 
-      const url = this.createElementWithTextNode('p', this.url, anchor);
+      icon.setAttribute('src', this.iconUrl);
 
-      url?.setAttribute('class', 'url');
+      anchor.appendChild(icon);
 
-      anchor?.appendChild(createSVGClipboard());
+      const url = this.createElementWithTextNode('p', this.url);
+
+      url.setAttribute('class', 'url');
+
+      anchor.appendChild(url);
+
+      anchor.appendChild(gitHubIcon());
+
+      return anchor;
     }
 
     /**
      * Create an HTMLElement with text.
      *
      * @param {string} tagName
-     * @param {string | null} data
-     * @param {HTMLElement | null} parentNode
-     * @returns {HTMLElement | null}
+     * @param {string | null} content
+     * @returns {HTMLElement}
      */
-    createElementWithTextNode(tagName, data = null, parentNode = null) {
-      if (!this.shadowRoot) {
-        return null;
-      }
-
+    createElementWithTextNode(tagName, content = null) {
       const element = document.createElement(tagName);
 
-      if (data) {
-        element.appendChild(document.createTextNode(data));
-      }
-
-      if (parentNode) {
-        parentNode.appendChild(element);
-      } else {
-        this.shadowRoot.appendChild(element);
+      if (content) {
+        element.appendChild(document.createTextNode(content));
       }
 
       return element;
